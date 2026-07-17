a43790d568ba07198ce366907fca734d372074bd
tylertab <159560470+tylertab@users.noreply.github.com>
2026-07-17 16:59:54 -0700

    Add: binary-search - submission-48

---
+++ Data Structures & Algorithms/binary-search/submission-48.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            # mid = (start + end) // 2  
+            # print(start, end)      
+            if nums[(start + end) // 2] == target:
+                return (start + end) // 2
+            if nums[(start + end) // 2] < target:
+                start = (start + end) // 2 + 1
+            else:
+                end = (start + end) // 2 - 1
+
+        return -1
