902636cbcc94324372574b70fef7bef3411b6597
tylertab <159560470+tylertab@users.noreply.github.com>
2026-08-04 00:55:39 -0700

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        def dfs(grid, i, j, dist):
+            if i not in range(len(grid)):
+                return None
+            if j not in range(len(grid[i])):
+                return None
+
+        
+            if grid[i][j] == -1:
+                return None
+            if grid[i][j] < dist:
+                return None
+            else:
+                grid[i][j] = dist
+            dfs(grid, i + 1, j, dist + 1)
+            dfs(grid, i - 1, j, dist + 1)
+            dfs(grid, i, j + 1, dist + 1)
+            dfs(grid, i, j - 1, dist + 1)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 0:
+                    dfs(grid,i,j,0)
+        
+                
