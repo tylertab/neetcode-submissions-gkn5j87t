32f2125ec0ce4ceac169f78dd296511ebcd1ed3b
tylertab <159560470+tylertab@users.noreply.github.com>
2026-08-10 02:01:06 -0700

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for citizen in details:
+            if int(citizen[11:13]) > 60:
+                count += 1
+        return count
