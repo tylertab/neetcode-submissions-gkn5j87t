0640524fa9a3323bf5745d8bd51404d2699a5098
tylertab <159560470+tylertab@users.noreply.github.com>
2026-08-22 22:35:32 -0400

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for log in logs:
+            if log == "./":
+                count = count
+            elif log == "../":
+                if count > 0:
+                    count -= 1
+            else:
+                count += 1
+
+        return count
