091b1182823694c1a321a1a74cd7f678c45874b6
tylertab <159560470+tylertab@users.noreply.github.com>
2026-08-31 09:52:53 -0400

    Add: minimum-changes-to-make-alternating-binary-string - submission-0

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        #option 1 10101
+        #option 2 0101010
+
+        def option(boo):
+            shouldbezero = boo
+            count = 0
+            for c in s:
+                print(shouldbezero,count)
+                if c == "0" and shouldbezero == False:
+                    count += 1
+                if c == "1" and shouldbezero == True:\
+                    count += 1
+                shouldbezero = not shouldbezero
+            return count
+        
+        zerostart = option(True)
+        onestart = option(False)
+        return min(zerostart, onestart)
+            
+                
